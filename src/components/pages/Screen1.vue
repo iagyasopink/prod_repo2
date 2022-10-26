76dcb1beb6754968a757596d1ec34f5c967aaf34
NUCAL <yagmur.sahin@nucal.com>
2022-10-26 11:13:01 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

--- src/components/pages/Screen1.vue
+++ src/components/pages/Screen1.vue
@@ -9,23 +9,32 @@
       v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
       v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
       slot="r0-c0" area="r0-c0">
-      <WImage v-bind:blank="false" v-bind:rounded="false" stretch="none"
-        imageUrl="https://wallpapers.com/images/hd/pokemon-snorlax-paw-print-1wzap1hyz1o6gyna.jpg"
+      <WImage top="0%" left="0%" v-bind:blank="false" v-bind:snapX="false" v-bind:snapY="false" v-bind:rounded="false"
+        stretch="none" imageUrl="https://wallpapers.com/images/hd/pokemon-snorlax-paw-print-1wzap1hyz1o6gyna.jpg"
+        position="absolute" v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black"
+        v-bind:aspectRatio="true" dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
+        ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
+        id="ddc04f73-a12e-4c6d-b7a8-3ecfc760448a"
+        v-bind:style="{'width':'522px','height':'278px','zIndex':2,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+      <WImage top="0%" left="56.52173913043478%" v-bind:blank="false" v-bind:snapX="false" v-bind:snapY="false"
+        v-bind:rounded="false" stretch="none"
+        imageUrl="https://wallpapers.com/images/hd/snorlax-paw-vector-art-a1q1o5ajqw8o7bof.jpg" position="absolute"
         v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black" v-bind:aspectRatio="true"
         dataLoading="static" ncTooltipText="Please write a text to display in tooltip..."
-        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
-        ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string" position="absolute"
-        top="0%" left="0%" v-bind:snapX="false" v-bind:snapY="false" id="ddc04f73-a12e-4c6d-b7a8-3ecfc760448a"
-        v-bind:style="{'width':'522px','height':'278px','maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':2}"/>
-      <WImage v-bind:blank="false" v-bind:rounded="false" stretch="none"
-        imageUrl="https://wallpapers.com/images/hd/snorlax-paw-vector-art-a1q1o5ajqw8o7bof.jpg" v-bind:isEnabled="true"
-        v-bind:thumbnail="false" blankColor="black" v-bind:aspectRatio="true" dataLoading="static"
-        ncTooltipText="Please write a text to display in tooltip..."
-        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
-        ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string" position="absolute"
-        top="0%" left="56.52173913043478%" v-bind:snapX="false" v-bind:snapY="false"
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
+        ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
         id="eaf1b100-e58d-45c9-b8d6-ed4fe530e5ab"
-        v-bind:style="{'width':'417px','height':'277px','maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px','zIndex':3}"/>
+        v-bind:style="{'width':'417px','height':'277px','zIndex':3,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#4B74FF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+      <WDefaultTextarea v-bind:lazy="false" rows="2" size="lg" text="" v-bind:trim="false" wrap="soft" value=""
+        v-bind:number="false" maxRows="" v-bind:disabled="false" v-bind:noResize="false" v-bind:readOnly="false"
+        v-bind:required="false" v-bind:autoFocus="false" dataModel="string" v-bind:plaintext="false"
+        textareaId="textarea" v-bind:ariaInvalid="false" placeholder="SNORLAX" ncTypography="custom"
+        v-bind:noAutoShrink="false" v-bind:lazyFormatter="false" ncTooltipText=""
+        v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'update':{'type':'dataModel'},'propName':'text','displayName':'Value'}]}"
+        v-bind:stateMinNumber="0" formatterFunctionName="" position="absolute" top="66.25194401244168%"
+        left="36.84782608695652%" id="32117314-5104-4c8f-a1da-3dbeb234d203"
+        v-bind:style="{'color':'#f24726','width':'386px','height':'168px','fontSize':'','maxWidth':'','minWidth':'','fontStyle':'NaN','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','background':'','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','marginLeft':'0px','paddingTop':'0px','visibility':'visible','borderColor':'#D1D1D6','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0.563px','textDecoration':'none','zIndex':4}"/>
     </NucGridCell>
   </NucGridPage>
 </template>
